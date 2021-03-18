d1c6ff813a13c623eeb9aa216465712652b8da1c
Akinori <aki10.working@gmail.com>
2021-03-19 00:16:03 +0900

    first upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper-container', { 
+    // Optional parameters
+    loop: true,
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $(function() {
+      var $header = $('header');
+      var $win = $(window);
+      var $cloneHeader = $header.clone().addClass('fixed-header').appendTo('body');
+      var showClass = 'is-show';
+
+      $win.on('load scroll', function() {
+          var value = $(this).scrollTop();
+          var triggerPos = 300;
+          if(value > triggerPos) {
+              $cloneHeader.addClass(showClass);
+          } else {
+              $cloneHeader.removeClass(showClass)
+          }
+      })
+  });
